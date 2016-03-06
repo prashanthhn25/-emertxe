af782ea252af51c943134b43fa783646f761c2d1
nanditha16 <nanditha.murthy@gmail.com>
2016-03-06 13:47:36 +0530

    modified CModule: 	added content for the program list modified LinuxModule: 	added LinuxContent which includes the list 	added 3 assignments in Assignments

---
+++ LinuxModule/Assignmnts/assignment1.sh
@@ -0,0 +1,5 @@
+#
+# Script for printing all the files related information in present working directory
+#
+
+ls -la
---
+++ LinuxModule/Assignmnts/assignment2.sh
@@ -0,0 +1,20 @@
+#
+# Read n and generate a pattern
+#   1
+#   12
+#   123
+#   1234
+#
+
+echo Enter n value:
+read n
+
+for (( i = 1; i <= n; i++ ))
+do
+	for (( j=1; j <= i; j++ ))
+	do
+		echo -n $j
+	done
+   	echo " "
+done
+
