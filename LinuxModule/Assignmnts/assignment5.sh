568f522784059b767370e37f6bb29d832b7c2a8a
nanditha16 <nanditha.murthy@gmail.com>
2016-03-06 14:47:05 +0530

    added assignment4, assignment5, assignment6

---
+++ LinuxModule/Assignmnts/assignment5.sh
@@ -0,0 +1,14 @@
+#
+# Script for addition of two numbers for real numbers
+#
+
+echo "Enter the first number:"
+read num1
+
+echo "Enter the second number:"
+read num2
+
+result=`echo "$num1+$num2" | bc`
+
+echo "addition of $num1 and $num2 is $result"
+
