efc507cbb383bbd48a210a6ddf3fc9d75852dd41
nanditha16 <nanditha.murthy@gmail.com>
2016-03-10 02:30:31 +0530

    Added files in Asignments/CModule

---
+++ CModule/Assignments/bitwise_lib.c
@@ -0,0 +1,31 @@
+/* Function Prototypes and Definitions to do bitwise
+ *  manipulations 
+ *  
+ *  functions in bitwise library.
+ *      -> int get_nbits(int num, int n);
+ *      -> int set_nbits(int num, int n, int val);
+ *      -> int get_nbits_from_pos(int num, int n, int pos);
+ *      -> int set_nbits_from_pos(int num, int n, int pos, int val);
+ *      -> int toggle_bits_from_pos(int num, int n, int pos);
+ *      -> int print_bits(unsigned int num, int n);
+ */
+
+/*
+ *  Fetch n number of bits from num and
+ *  return it to the called function.
+ *
+ *  Example: If num is 10 and n is 2,
+ *      10 ->	0 0 0 0 1 0 1 0
+ *                          ---
+ *
+ *  Take 2 bits from LSB end of 10 (1 0) and 
+ *  return the corresponding decimal of that.
+ *
+ *  So get_nbits(10, 2) function should return 2
+ *
+ */
+int get_nbits(int num, int n)
+{
+    
+    return 0;
+}
