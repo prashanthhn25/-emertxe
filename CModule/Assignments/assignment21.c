059b86bc6db5e694e05dcab3d27f9ff05012f053
nanditha16 <nanditha.murthy@gmail.com>
2016-03-17 22:12:59 +0530

    Modified assignment21 in CModule

--- CModule/Assignments/assignment21.c
+++ CModule/Assignments/assignment21.c
@@ -26,53 +26,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int main()
 {
-    int n, returnValue;
-    static char buf[16];
-    
-    char c;
+    static int returnValue, flag = 1, num, n, temp =1;
+	
+	if(flag)
+    {
+        printf("Enter n: ");
+        scanf("%d", &num);
+		flag = 0;
+	}	
+	
+	temp = temp * num--;
 
-        /* Do arg count check */
-        if (argc < 2)
-        {
-            printf("Enter n: ");
-            scanf("%d", &n);
-        }
-        else
-        {
-            /* get the int value from argv */
-            n = atoi(argv[1]);
-        }
-
-        /* check for initial valid number */
-        if (n <= 0)
-        {
-            returnValue = 1;
-        }
-        else
-        {
-            if (n == 1)
-            {
-                returnValue = 1;
-            }
-            else
-            {
-                sprintf(buf, "%d", n - 1);
-                /* pt is the pointer to the value in buf */
-                char *pt = buf;
-                
-                /* take the value stored in address of previous value (n-1) */
-                char **pt2 = &pt - 1;
-                
-                /* Call main function from main for calculating factorial */
-                returnValue = main(2, pt2) * n;
-            }
-            
-        }
-        printf("Factorial of %d: %d \n", n, returnValue);
-
-        return returnValue;
+	/* check for initial valid number */
+    if (num < 1)
+    {
+		printf("Factorial: %d \n", temp);
+        return 0;
+    }
+    else
+    {
+		main();
+	}
     
 }
 
