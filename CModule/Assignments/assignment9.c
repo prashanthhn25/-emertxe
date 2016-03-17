ae6cdb0b812e9bc4d43989bf0b564adc960e5ddc
nanditha16 <nanditha.murthy@gmail.com>
2016-03-17 22:04:40 +0530

    Modified assignment9 in CModule

--- CModule/Assignments/assignment9.c
+++ CModule/Assignments/assignment9.c
@@ -10,25 +10,47 @@ int main()
 {
     
     int dayNumber;
+	int startDay;
     
     /* days */
     char *days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
     
-    
-    const unsigned short int __mon_yday[2][13] =
-    {
-        /* Normal years.  */
-        { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 },
-        /* Leap years.  */
-        { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 }
-    };
-    
+     /* Enter the starting day of the year */
+    printf("Enter the starting day of the year: \n");
+	printf("1. Monday \n2. Tuesday \n3.Wednesdday \n4. Thursdday \n5. Friday \n6. Saturday \n7. Sunday\n ");
+	printf("StartDay: \n");
+    scanf("%d", &startDay);
+	
     /* Enter the number */
     printf("Enter the number between 1-365 (incl): ");
     scanf("%d", &dayNumber);
     
-    dayNumber%=7;
-    printf("Day is : %s\n", *(days+dayNumber));
-    
+	/* Print the day accordingly */
+	switch (startDay)
+	{
+		case 1: dayNumber%=7; /* Print the day accordingly for start day being Monday */
+		        printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		case 2: dayNumber = (dayNumber+1) % 7;
+				printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		case 3: dayNumber = (dayNumber+2) % 7;
+				printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		case 4: dayNumber = (dayNumber+3) % 7;
+				printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		case 5: dayNumber = (dayNumber+4) % 7;
+				printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		case 6: dayNumber = (dayNumber+5) % 7;
+				printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		case 7: dayNumber = (dayNumber+6) % 7;
+				printf("Day is : %s\n", *(days+dayNumber));
+				break;
+		default: printf("Invalid Option \n");
+				break;
+	}
     return 0;
 }	
