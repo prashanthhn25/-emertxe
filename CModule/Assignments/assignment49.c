089f990f6d29f576e99669c0e597a68633ec190f
nanditha16 <nanditha.murthy@gmail.com>
2016-03-31 12:15:25 +0530

    Added assignment 48, 49 in CModule 	assignment49: Incomplete

---
+++ CModule/Assignments/assignment49.c
@@ -0,0 +1,43 @@
+/*
+ * Q-19
+ *      Take n and k ( 1 <= k <= 10) as i/p. 
+ *  Generate consecutive NRPS of len n using k distinct char
+ * ( 0 <= k <=9 )
+ *
+ *  Explaination :
+ *  1. Suppose k = 3 ( say taking 3 distinct characters )
+ *      Let 3 distinct characters be a , b , c
+ *  2. Suppose  'n' is the string length to be formed using
+ *      'k' distinct words.
+ *      Let n be 6
+ *	3. The string should be formed in such a way that there should 
+ *      not be any consecutive repetitions of the strings.
+ *
+ *  Example 1:
+ *      If k = 3  & n = 6
+ *      Valid NRPS string = abcacb
+ *      Invalid NRPS string = abcabc ( since “ abc ” is occurring 
+ *          consecutively)
+ *  Example 2:
+ *      If k = 3 & n = 5
+ *      valid NRPS string = abcab
+ *
+ *  Sample run :
+ *
+ *      Input    : Enter the value of k , n and “ 
+ *      k disctint characters” 3  6 a b c
+ *	Output :  NRPS string = abcacb ( or any combination of 3
+ *      distinct characters, where the string length is n = 6)
+ *
+ *
+ */
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
