4789b87fda1ce6923b3232e4860cff56786e0862
nanditha16 <nanditha.murthy@gmail.com>
2016-04-17 14:35:54 +0530

    Modified assignment55 in CModule 	assignment55: Modified demote to int

--- CModule/Assignments/assignment55.c
+++ CModule/Assignments/assignment55.c
@@ -11,27 +11,55 @@
 	
 	struct _FLOATRANGE
 	{
-		unsigned MANTESSA :23;
-		unsigned EXPONENT :8;
+		unsigned int MANTESSA :23;
+		unsigned int EXPONENT :8;
 		unsigned char SIGN :1;
 		
 		 
 	} floatRange;
 	
  } floatNum;
+
 #include <stdio.h>
 
 int main()
 {
-    float fx;
+  
+    int exp, mask;
     
     /* Swap int values */
     printf("Enter a float value: ");
-    scanf("%f", &fx);
+    scanf("%f", &floatNum.FLOATNUM );
     
-	floatNum.FLOATNUM = fx;
 	
-    //printf("float actual value: %d \n", floatNum.floatRange.EXPONENT);
-    printf("demoted int value: %.f \n", fx);
+    //printf("sign = %#x\n", floatNum.floatRange.SIGN);
+    //printf("exponent: %d \n", floatNum.floatRange.EXPONENT);
+    //printf("mantissa: %#x \n", floatNum.floatRange.MANTESSA);
+    //printf("float = %f\n", floatNum.FLOATNUM);
+    
+    
+    if (floatNum.floatRange.EXPONENT >= 127)
+    {
+        exp = floatNum.floatRange.EXPONENT - 127;
+       // printf("exp = %d\n", exp);
+        
+        mask = (~((1 << (23 - exp)) - 1));
+        //printf("mask = %#x\n", mask);
+        
+        floatNum.floatRange.MANTESSA &= mask;
+        //printf("mantissa: %#x \n", floatNum.floatRange.MANTESSA);
+        
+        printf("demoted int value: %f \n",  floatNum.FLOATNUM);
+        
+    }
+    else
+    {
+        exp = 127 - floatNum.floatRange.EXPONENT;
+        floatNum.floatRange.SIGN &= 0;
+        floatNum.floatRange.EXPONENT &= 0;
+        floatNum.floatRange.MANTESSA &= 0;
+        printf("demoted int value: %f \n",  floatNum.FLOATNUM);
+    }
+   
     return 0;
 }
